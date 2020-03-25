06d1e88ae9ae0c1ed76ea05ecc91d0716a59f838
Mariia Kryvokin <kruvokin@gmail.com>
2020-03-25 18:11:23 +0200

    bridge

--- structural/src/bridge/Main.java
+++ structural/src/bridge/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Device device = new TV(Collections.singletonList("first channel"));
+        Device electroCar = new ElectroCar();
         Remote remote = new Remote(device);
         System.out.println(remote.turnOn());
+        remote.setDevice(electroCar);
+        System.out.println(remote.turnOn());
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.doSomethingSpesial();
     }
 }
