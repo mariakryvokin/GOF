a2de65ae3f984ac09b305399283add317f433aa6
Mariia Kryvokin <kruvokin@gmail.com>
2020-03-26 13:16:29 +0200

    delete odd classes from main class in singleton

--- singleton/src/Main.java
+++ singleton/src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
 
 
-    static class A{
-        public final void lol(){
-            System.out.println("Lol");
-        }
-    }
-
-    static class B extends A{
-
-    }
     public static void main(String[] args) {
         Datasource datasource = Datasource.getInstance();
         Datasource anotherDatasource = Datasource.getInstance();
@@ -17,6 +8,5 @@ public class Main {
         Singleton singleton = Singleton.getInstance();
         Singleton anotherSingleton = Singleton.getInstance();
         System.out.println(singleton == anotherSingleton);
-        new B().lol();
     }
 }
